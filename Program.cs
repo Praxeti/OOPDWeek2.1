efb3e2a7678faa44bb4616aa56587399a56db134
Praxeti <cowsruleman@gmail.com>
2019-10-04 14:13:28 +0000

    Added code, made program

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace OOPDWeek2
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Creating an instance of parent");
+        }
+        
+        public void Print()
+        {
+            Console.WriteLine("I am a parent!");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Creating an instance of child");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("I am a child!");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            ParentClass parent = new ParentClass();
+            ChildClass child = new ChildClass();
+
+            parent.Print();
+            child.SayHello();
+        }
+    }
+}
